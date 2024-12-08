2527497f966595a4024082b4dc63b1624648d213
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2024-12-08 19:06:25 +0600

    added all functionality

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-function letsChange() {}
+const body = document.getElementById('body');
+const letColor = document.getElementById('letColor');
+
+function letsChange() {
+  body.style.background = `${letColor.value}`;
+  console.log(letColor.value);
+}
+
+setInterval(() => {
+  letsChange();
+}, 200);
